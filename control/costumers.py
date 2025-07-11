549ac6ce84cdc28c81685a412594b3f222d51174
NYSD DEREK <124192421+nysd01@users.noreply.github.com>
2025-07-12 00:38:55 +0100

    going crazy with deployment

---
+++ control/costumers.py
@@ -0,0 +1,14 @@
+# control/consumers.py
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+
+class MyConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+        await self.send(text_data=json.dumps({"message": "WebSocket connected"}))
+
+    async def disconnect(self, close_code):
+        pass
+
+    async def receive(self, text_data):
+        await self.send(text_data=json.dumps({"message": "You said: " + text_data}))
